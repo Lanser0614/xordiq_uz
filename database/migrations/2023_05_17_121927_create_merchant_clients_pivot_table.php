3a8326c53ee883c7cb21881727d90eb03c12e1d8
lanser <a.doniyor@aliftech.uz>
2023-05-18 10:30:35 +0500

    add env.ci

--- database/migrations/2023_05_17_121927_create_merchant_clients_pivot_table.php
+++ database/migrations/2023_05_17_121927_create_merchant_clients_pivot_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('merchant_clients_pivot', function (Blueprint $table) {
             $table->integer('merchant_id');
@@ -26,7 +26,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('merchant_clients_pivot');
     }
